086f3b01e532d04110713085da2588d386d0670f
Peter Stokes <peter.stokes@ephe.psl.eu>
2025-10-13 17:52:07 +0200

    Update build-iipsrv.sh
    Pinned iipsrv to v1.2 since v1.3 doesn't build in this environment.

--- build/build-iipsrv.sh
+++ build/build-iipsrv.sh
@@ -1,6 +1,8 @@
 cd /tmp
 git clone https://github.com/ruven/iipsrv.git
 cd iipsrv
+# Version 1.3 no longer builds in the current Docker image
+git checkout iipsrv-1.2
 # Deployed in /tmp/iipsrv/src/iipsrv.fcgi
 ./autogen.sh && ./configure && make
 mv /tmp/iipsrv/src/iipsrv.fcgi /usr/local/bin/.
